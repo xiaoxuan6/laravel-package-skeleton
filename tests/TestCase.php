f0dfff331dc81db47d24b6b48110e42f8088a202
xiaoxuan6 <1527736751@qq.com>
2022-02-16 21:28:53 +0800

    init

---
+++ tests/TestCase.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) vinhson <15227736751@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Vinhson\LaravelPackageSkeleton\Tests;
+
+use Illuminate\Foundation\Application;
+use Vinhson\LaravelPackageSkeleton\LaravelPackageSkeletonServiceProvider;
+
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param Application $app
+     *
+     * @return array<int, class-string>
+     */
+    protected function getPackageProviders($app): array
+    {
+        return [
+            LaravelPackageSkeletonServiceProvider::class
+        ];
+    }
+}
