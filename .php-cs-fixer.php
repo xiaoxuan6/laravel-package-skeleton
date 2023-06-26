0325bc2654a32b680f5147578eada9d9bd3e2462
xiaoxuan6 <1527736751@qq.com>
2023-06-26 15:23:09 +0800

    fix: remove php cs fixer action

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -60,7 +60,7 @@ return (new PhpCsFixer\Config())
         'return_assignment' => true,
         'blank_line_after_namespace' => true,
         'no_leading_namespace_whitespace' => true,
-        'single_blank_line_before_namespace' => true,
+//        'single_blank_line_before_namespace' => true,
         'fully_qualified_strict_types' => true,
         'global_namespace_import' => [
             'import_classes' => true,
